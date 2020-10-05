e0c672b569a2c345bffec83b7d525bde73b2ecd8
Siavash Moeini <smoeinij@asu.edu>
2020-10-05 16:51:11 -0700

    Initial Commit

---
+++ src/cse360assignment02/AddingMachine.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cse360assignment02;
+/**
+ *
+ * Name: Siavash Moeini
+ * Student id: 1217773375
+ * Class: CSE 360
+ * Date: 10/05/2020
+ * Purpose: This program is designed for the students to get familiar with github repository.
+ * 
+ */
+public class AddingMachine {
+    private int total;
+  
+  public AddingMachine () {
+    total = 0;  // not needed - included for clarity
+  }
+  
+  public int getTotal () {
+    return 0; 
+  }
+  
+  public void add (int value) {
+  }
+
+  public void subtract (int value) {
+  }
+
+  public String toString () {
+    return "";
+  }
+
+  public void clear() {
+  }
+    
+}
